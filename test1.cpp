03c30b06d8e3838fd39e6d5c4b60d7df3ce26b20
Gabriel <gabrielrstella@gmail.com>
2022-01-21 22:31:05 -0600

    more tests

--- test1.cpp
+++ test1.cpp
@@ -8,12 +8,52 @@ using std::chrono::duration;
 
 typedef unsigned long long Jimmy;
 
+struct test {
+	int x;
+	int y;
+};
+
+class test2 {
+	int x; //this is private
+
+	public:
+
+	int y; //this is public
+
+	//this is public
+	test2(int val) : x(val), y(val + 1) {}
+
+	//this is public
+	int getX() {
+		return x;
+	}
+
+	private:
+
+	//this is private
+	int setX(int x) {
+		this->x = x;
+	}
+};
+
+void func() {
+
+	test t;
+	t.x;
+	test* tpointer = &t;
+	(*tpointer).x;
+	tpointer->x;
+}
+
+
 int main (int argc, char** argv) {
 	printf("test1 run with %d cmdline arguments\n", argc);
 
 	//thanks to the above typedef, these mean the same thing
 	unsigned long long x1 = 1234;
 	Jimmy x2 = 1234;
+
+	char* string = "hello";
 
 	//do some other fun stuff
 
@@ -25,7 +65,7 @@ int main (int argc, char** argv) {
 
 			//buffer copying
 
-			Jimmy sz = 1LLU << 24;
+			Jimmy sz = 1LLU << 24; //16 MB
 			char* src = new char[sz];
 			char* dst = new char[sz];
 
@@ -58,12 +98,17 @@ int main (int argc, char** argv) {
 			int z = 3333;
 			//
 			int* p = &y;
+			auto dist = (int*)(&p) - p;
 			//
 			int a = x + z;
 			int b = y + z;
 			int c = a + b;
 			//
-			printf("y (%lu) is at %p; &y (%lu) is at %p; *p = %d\n", sizeof(y), (void*)p, sizeof(p), (void*)&p, *p);
+			printf("y (%lu) is at %p; &y (%lu) is at %p (+%ld); *p = %d\n", sizeof(y), (void*)p, sizeof(p), (void*)&p, dist, *p);
+			//
+			int lower = int(Jimmy(p)) & 0xFFFFFFFF;
+			int upper = (Jimmy(p) >> 32) & 0xFFFFFFFF;
+			printf("upper = %d, lower = %d\n", lower, upper);
 
 			for(int i = 0; i < 4; i++) {
 				printf("*(p+%d) = %d\n", i, *(p+i));
